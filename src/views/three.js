e45557b52c7b17d8eb4e39b87aa2b9aa5fb8fc31
samelie <sam@stinkdigital.com>
2017-06-09 19:21:41 -0700

    deux bundle

--- src/views/three.js
+++ src/views/three.js
@@ -35,7 +35,7 @@ const videoEl = ({ video, onload }) => (
     onload=${onload}
     width="640"
     height="360"
-    src=http://youtube.com/embed/${video.id.id}?rel=0&showinfo=0&enablejsapi=1&wmode=transparent&version=3&playerapiid=ytplayer&loop=1&modestbranding=true
+    src=https://youtube.com/embed/${video.id.id}?rel=0&showinfo=0&enablejsapi=1&wmode=transparent&version=3&playerapiid=ytplayer&loop=1&modestbranding=true
     frameborder="0"
   ></iframe>`
 )
